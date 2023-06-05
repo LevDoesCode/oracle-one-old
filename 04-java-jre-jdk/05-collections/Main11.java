8198b3335527ffc61637f085f798e182363e32fe
levdoescode <levdoescode+sites@gmail.com>
2023-06-05 15:12:30 -0500

    Worked with stream() and lambda. Removed element from HashSet

--- 04-java-jre-jdk/05-collections/Main11.java
+++ 04-java-jre-jdk/05-collections/Main11.java
@@ -1,7 +1,8 @@
 package collections;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.Set;
 
 public class Main11 {
 
@@ -15,7 +16,7 @@ public class Main11 {
 		String student5 = "Gustav";
 		String student6 = "Foxie";
 
-		Set<String> listStudents = new HashSet<>();
+		Collection<String> listStudents = new HashSet<>(); // HashSet can be set as Collection
 
 		listStudents.add(student1);
 		listStudents.add(student2);
@@ -23,7 +24,25 @@ public class Main11 {
 		listStudents.add(student4);
 		listStudents.add(student5);
 		listStudents.add(student6);
+		listStudents.add(student6); // HashSet doesn't add duplicates
 
+		for (String student : listStudents) {
+			System.out.println(student);
+		}
+
+		// listStudents.get() // not supported by HashSet
+
+		System.out.println(listStudents.contains("Foxie"));
+		System.out.println(listStudents.contains("FOXIE"));
+
+		String longest = listStudents.stream().max(Comparator.comparingInt(String::length)).get();
+		System.out.println("---Longest---");
+		System.out.println(longest);
+		System.out.println(longest.length());
+
+		// using lambda
+		System.out.println("---Remove longest---");
+		listStudents.removeIf(student -> longest.equalsIgnoreCase(student));
 		for (String student : listStudents) {
 			System.out.println(student);
 		}
