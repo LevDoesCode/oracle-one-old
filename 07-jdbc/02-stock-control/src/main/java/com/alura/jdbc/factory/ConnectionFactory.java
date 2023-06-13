f5c54e1c0766bf5139edfba9a6728e17d6bb6e95
levdoescode <levdoescode+sites@gmail.com>
2023-06-12 20:02:08 -0500

    Used the pooled connection to return connections

--- 07-jdbc/02-stock-control/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ 07-jdbc/02-stock-control/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,14 +1,28 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
-	public Connection GetConnection() throws SQLException {
+
+	private DataSource dataSource;
+
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
 		String url = "jdbc:mysql://localhost/stock_control?useTimeZone=true&serverTimeZone=UTC";
 		String user = "root";
 		String pass = "SECUREPASS";
-		return DriverManager.getConnection(url, user, pass);
+		pooledDataSource.setJdbcUrl(url);
+		pooledDataSource.setUser(user);
+		pooledDataSource.setPassword(pass);
+		this.dataSource = pooledDataSource;
+	}
+
+	public Connection GetConnection() throws SQLException {
+		return this.dataSource.getConnection();
 	}
 }
